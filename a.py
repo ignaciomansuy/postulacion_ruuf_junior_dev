c9f5e9e16a7c9d93a2a22cdf1535f7ff6252040b
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2024-04-21 12:56:29 -0400

    feat: getting optimal solution for all tests

--- a.py
+++ a.py
@@ -3,40 +3,44 @@ def grid_size(horizontal_length: int, vertical_length: int, x: int, y: int, cx: 
   return (x // horizontal_length - cx) * (y // vertical_length - cy)
 
 
-def get_rects_right(horizontal_length: int, vertical_length: int, x: int, y: int, c: int=0):
+def get_rects_right_to_bottom(horizontal_length: int, vertical_length: int, x: int, y: int, c: int=0):
   return grid_size( vertical_length, horizontal_length,
                     x - horizontal_length * (x // horizontal_length - c),
-                    vertical_length * (y // vertical_length))
+                    y)
   
-def get_rects_bottom(horizontal_length: int, vertical_length: int, x: int, y: int, c: int=0):
+def get_rects_bottom(horizontal_length: int, vertical_length: int, x: int, y: int, cx: int=0, cy: int=0):
   return grid_size( vertical_length, horizontal_length,
-                    x,
-                    y % vertical_length + c * vertical_length)
+                    horizontal_length * (x // horizontal_length - cx),
+                    y % vertical_length + cy * vertical_length)
 
 
 def helper(horizontal_length: int, vertical_length: int, x: int, y: int):
   # All optimal solutions are made of 1 big grid, and then 0 or 1 column and/or row of little rects rotated in 90°
   big_grid = grid_size(horizontal_length, vertical_length, x, y)
-  rects_right_big_grid = get_rects_right(horizontal_length, vertical_length, x, y)
-  rects_bottom_big_grid = get_rects_bottom(horizontal_length, vertical_length, x, y)
   
-  rects_with_big_grid = rects_right_big_grid + rects_bottom_big_grid + big_grid
+  grid_minus_1_horizontally = grid_size(horizontal_length, vertical_length, x, y, cx=1)
+  grid_minus_1_vertically = grid_size(horizontal_length, vertical_length, x, y, cy=1)
   
-  # There are some cases were the optimal solution requires to not put the biggest initial grid
+  amounts_of_columns = x // horizontal_length
+  amounts_of_rows = y // vertical_length 
   
-  rects_right_small_grid = get_rects_right(horizontal_length, vertical_length, x, y, c=1)
-  rects_bottom_small_grid = get_rects_bottom(horizontal_length, vertical_length, x, y, c=1)
+  if (x - horizontal_length * amounts_of_columns) // vertical_length == 0 and \
+      (x - horizontal_length * (amounts_of_columns - 1)) // vertical_length >= 1:
+      grid_minus_1_horizontally += get_rects_right_to_bottom(horizontal_length, vertical_length, x, y, c=1)
+      
+  temp = get_rects_right_to_bottom(horizontal_length, vertical_length, x, y)
+  big_grid += temp
+  grid_minus_1_vertically += temp
   
+  if (y - vertical_length * amounts_of_rows) // horizontal_length == 0 and \
+      (y - vertical_length * (amounts_of_rows - 1) // horizontal_length >= 1):
+      grid_minus_1_vertically += get_rects_bottom(horizontal_length, vertical_length, x, y, cy=1)
   
-  a = [rects_with_big_grid,
-    grid_size(horizontal_length, vertical_length, x, y, cx=1) + rects_right_small_grid + rects_bottom_big_grid,
-    grid_size(horizontal_length, vertical_length, x, y, cy=1) + rects_right_big_grid + rects_bottom_small_grid,
-    grid_size(horizontal_length, vertical_length, x, y, cx=1, cy=1) + rects_right_small_grid + rects_bottom_small_grid,]
-    
+  temp = get_rects_bottom(horizontal_length, vertical_length, x, y)
+  big_grid += temp
+  grid_minus_1_horizontally += temp
   
-  return max(
-    a
-  )
+  return max(big_grid, grid_minus_1_horizontally, grid_minus_1_vertically)
   
 
 
@@ -45,10 +49,8 @@ def max_amount_of_rects_in_rect(a: int, b: int , x: int, y: int):
   option1 = helper(a, b, x, y)
   option2 = helper(b, a, x, y)
   
-  return option1 if option1 >= option2 else option2
+  return max(option1, option2)
+
       
     
-  
-      
-    
-max_amount_of_rects_in_rect(16, 21, 100, 140)
+max_amount_of_rects_in_rect(16, 21, 105, 133)
